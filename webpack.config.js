c59c5973f964bc84cde1532e563004b1fc55aef4
Evgeniy Martynov <evgen.m@fireart-d.com>
2019-04-30 14:34:52 +0300

    main app

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+
+var path = require('path');
+
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'foo.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ]
+};
+
+
+
+/*
+
+ // webpack.config.js
+ const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+ module.exports = {
+ mode: 'development',
+ module: {
+ rules: [
+ {
+ test: /\.vue$/,
+ loader: 'vue-loader'
+ },
+ // this will apply to both plain `.js` files
+ // AND `<script>` blocks in `.vue` files
+ {
+ test: /\.js$/,
+ loader: 'babel-loader'
+ },
+ // this will apply to both plain `.css` files
+ // AND `<style>` blocks in `.vue` files
+ {
+ test: /\.css$/,
+ use: [
+ 'vue-style-loader',
+ 'css-loader'
+ ]
+ }
+ ]
+ },
+ plugins: [
+ // make sure to include the plugin for the magic
+ new VueLoaderPlugin()
+ ]
+ }
+
+
+*/
